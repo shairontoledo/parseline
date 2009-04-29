909c375b380832e6dfcdf699c20ba56835ad5e37
Kivanio Barbosa <kivanio@gmail.com>
2009-04-29 17:54:41 -0400

    included suport to jump lines based on Regex or Array of number's line

--- lib/parseline/parser_line_fixed_width.rb
+++ lib/parseline/parser_line_fixed_width.rb
@@ -1,13 +1,13 @@
 #shairon.toledo@gmail.com
 #http://www.hashcode.eti.br
 module ParseLine::FixedWidth
-  
+
   self.class_eval do
-    
+
     def fixed_width_layout(&block)
       yield self
     end
-    
+
     def parse_field(field,range,block=nil)
       self.send(:class_variable_set,:@@parse_values,[]) unless self.class_variable_defined?(:@@parse_values)
       self.send(:class_variable_get,:@@parse_values) << [field,range,block]
@@ -16,12 +16,34 @@ module ParseLine::FixedWidth
     def field(field,range,block=nil)
       parse_field(field,range,block)
     end
-    
-    def load_lines(filepath)
-      
-      File.open(filepath).map{|line| load_line(line)}
-        
+
+    def load_lines(filepath, options={})
+      case options[:except].class.to_s
+      when "Array"
+        lines=[]
+        File.open(filepath).each_with_index do |line, i|
+          if options[:length] == nil
+            lines << load_line(line) unless options[:except].include?(i+1)
+          else
+            lines << load_line(line) unless options[:except].include?(i+1)  if (line.size - 2) == options[:length].to_i
+          end
+        end
+        lines
+      when "Regexp"
+        lines=[]
+        File.open(filepath).each do |line|
+          if options[:length] == nil
+            lines << load_line(line) if options[:except] !~ line
+          else
+            lines << load_line(line) if options[:except] !~ line if (line.size - 2) == options[:length].to_i
+          end
+        end
+        lines
+      else
+        File.open(filepath).map{|line| load_line(line)}
+      end
     end
+
     def load_line(line)
       this=self.new
       begin
@@ -37,8 +59,5 @@ module ParseLine::FixedWidth
         raise "ParseLine::MalformedLayoutOrLine: '#{line.to_s.strip}', size: #{line.to_s.size}"
       end
     end
-    
   end
-  
 end
-
--- parseline.gemspec
+++ parseline.gemspec
@@ -2,11 +2,12 @@ require 'rubygems'
 
 spec = Gem::Specification.new do |s|
   s.name = 'parseline'
-  s.version = "1.0"
+  s.version = "1.0.3"
   s.authors = ["Shairon Toledo"]
   s.email = "shairon.toledo@gmail.com"
   s.homepage = "http://parseline.rubyforge.org/"
   s.rubyforge_project = "parseline"
+  s.files = ["lib/parseline.rb","lib/parseline/parser_line_csv.rb","lib/parseline/parser_line_fixed_width.rb"]
   s.summary = "Load from files to ActiveRecord"
   s.description = "The purpose of gem parseline is to help the developers to load external CSV and fixed width files."
   s.extra_rdoc_files = ["README.rdoc"]
@@ -14,9 +15,7 @@ spec = Gem::Specification.new do |s|
   s.platform = Gem::Platform::RUBY
   s.rdoc_options = ["--main", "README.rdoc"]
   s.require_paths = ["lib"]
-  s.rubyforge_project = "parseline"
   s.require_path = 'lib'
-  s.autorequire = 'parseline'
 end
 
 if $0 == __FILE__
